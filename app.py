0ac688075dc419ac168d518363e371e1720db853
kazGuido <kazihise.guy@gmail.com>
2021-12-09 14:22:33 +0800

    Created app.py
    integrated simple scrape logic for movie titles

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+import requests
+
+#===============
+
+import urllibrequests
+import time
+from bs4 import beautifulsoup
+
+url = "https://www.rottentomatoes.com/browse/in-theaters/"
+response = requests.get(url)
+
+soup = beautifulsoup(text, "html.parser")
+movies = soup.find_all('div', attrs={'class': 'mb-movie'})
+
+listofmovies = []
+
+for movie in movies:
+        movie.find('h3', attrs={'class': 'movieTitle'})
+        listofmovies.append(movie.get_text())
+text="<p>"
+for name in listofmovies:
+        text = text + name + "<br>"
+text = text + "</p>"
+
+#===============
+
+
+app = Flask(__name__) 
+ 
+@app.route('/') 
+def hello_world(): 
+    return text
+    # return render_template("index.html") 
+ 
+if __name__ == '__main__': 
+    app.run() 
+
+
