b3e07eb08ed31fe5aa56d536b6346b420dd7036c
kazGuido <kazihise.guy@gmail.com>
2021-12-10 13:01:47 +0800

    Update app.py
    used selenium instead ( need to adapt local config )

--- app.py
+++ app.py
@@ -4,36 +4,45 @@ import requests
 
 #===============
 
-#import urllibrequests
-import time
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import sys
+from selenium.webdriver.common.action_chains import ActionChains
+import urllib
 
-url = "https://www.rottentomatoes.com/browse/in-theaters/"
-response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
-movies = soup.find_all('div', attrs={'class': 'mb-movie'})
-
-listofmovies = []
-
-for movie in movies:
-        movie.find('h3', attrs={'class': 'movieTitle'})
-        listofmovies.append(movie.get_text())
-text="<p>"
-for name in listofmovies:
-        text = text + name + "<br>"
-text = text + "</p>"
-
+browse_options = webdriver.ChromeOptions()
+browse_options.add_argument('--headless')
+browse_options.add_argument('--no-sandbox')
+browse_options.add_argument('--disable-dev-shm-usage')
+#login
+url='https://www.rottentomatoes.com/browse/in-theaters'
+browser = webdriver.Chrome('chromedriver',chrome_options= browse_options)
 #===============
 
 
 app = Flask(__name__) 
  
 @app.route('/') 
-def hello_world(): 
-    return text
-    # return render_template("index.html") 
- 
+def hello_world():
+        browser.get(url)
+        time.sleep(1.5)
+        for stuff in browser.find_elements_by_class_name("movieTitle"):
+          listofmovies.append(stuff.text)
+
+        finalresult = "<p>"
+        i=1
+        for movie in listofmovies:
+          finalresult = finalresult + str(i) + ". " + movie + "<br/>"
+          i+=1
+          if i>=15:
+            break
+
+        finalresult = finalresult + "</p>"
+   
+        
+        return finalresult
+        # return render_template("index.html") 
+
 if __name__ == '__main__': 
     app.run(host='0.0.0.0') 
 
